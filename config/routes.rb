b4a8ae2be7bd7f3165afc23893421ac59709e516
lapvv <lapvv@yandex.ru>
2023-06-02 16:26:10 +0800

    wip app

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  # Defines the root path route ("/")
+  root "static_pages#home"
+  get 'static_pages/home'
+  get 'static_pages/help'
+  get 'static_pages/about'
   resources :microposts
   resources :users
-
-  # Defines the root path route ("/")
-  root "users#index"
 end
