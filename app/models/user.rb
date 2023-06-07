c38b70c8f1a9353f1db851f35526ba943c4aa714
lapvv <lapvv@yandex.ru>
2023-06-07 17:58:58 +0800

    users fields validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
     has_many :microposts
-    validates :name, presence: true
-    validates :email, presence: true
+    before_save { self.email = email.downcase}
+    validates :name, presence: true, length: { maximum:50 }
+    validates :email, presence: true, length: { maximum:255 }, uniqueness: true,
+        format: { with: /\A[\wа-яА-я+\-.]+@[a-z\d\-.]+[\.]{1}[a-zа-я]+\z/i }
+    validates :password, presence: true, length: { minimum:6 }
+    has_secure_password
 end
